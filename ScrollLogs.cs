7d5e87338ed8435e5e796f5292636e18b6007950
flatworldstudio <jasper@flatworldstudio.nl>
2020-08-04 14:06:39 +0200

    scrollable log

---
+++ ScrollLogs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace StoryEngine
+{
+    public class ScrollLogs : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetPosition(float value)
+        {
+            transform.localPosition = new Vector3(-0.5f*Screen.width,(-0.5f+ value * 4)* Screen.height, 0);
+
+        }
+    }
+}
